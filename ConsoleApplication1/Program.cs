0ac5c206b31dec2c557cd626c3b8302aad70880e
turnip99 <turnerma101265@aol.com>
2015-09-23 10:52:16 +0100

    Comments as requested

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,10 +10,15 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            // this is a turnip with 11.
             Console.WriteLine("Turnip 11");
+            // this is a turnip with 12
             Console.WriteLine("Turnip 12");
+            // this is a turnip with 13
             Console.WriteLine("Turnip 13");
+            // this is a turnip with 14
             Console.WriteLine("Turnip 14");
+            // this lets you read the turnips
             Console.ReadLine();
         }
     }
